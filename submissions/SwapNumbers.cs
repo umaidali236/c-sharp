eb8418a02b129d1ec6ea03e7b2f7989ffd686859
Umaid Ali <124885669+umaidali236@users.noreply.github.com>
2024-05-18 22:35:34 +0530

    Update SwapNumbers.cs

--- submissions/SwapNumbers.cs
+++ submissions/SwapNumbers.cs
@@ -2,7 +2,7 @@
 // Assignment-2 #Program to swap two numbers
 
 Console.WriteLine("Enter the value of a  : ");
-int a = Convert.ToInt32 (Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the value of b  : ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Before swapping : \n a = {a} \n b = {b}" );
