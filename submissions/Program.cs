17c4e247df79a2bb01adfdac7c1e0a126a5a9520
umaidali236 <umaid236@gmail.com>
2024-05-18 19:18:53 +0530

    Update Program.cs

--- submissions/Program.cs
+++ submissions/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Assignment-1
 
-Console.WriteLine("Hello there ! ");
+Console.WriteLine("Hello there ! \n");
 
-Console.WriteLine(" \n This course serves as an introduction to the C# programming language, one of the most widely used languages for developing desktop, web, and mobile applications on the .NET framework.We will learn the fundamentals of C# syntax, data types, control structures, object-oriented programming concepts and common programming techniques.");
+Console.WriteLine(" This course serves as an introduction to the C# programming language, one of the most widely used languages for developing desktop, web, and mobile applications on the .NET framework.We will learn the fundamentals of C# syntax, data types, control structures, object-oriented programming concepts and common programming techniques.\n");
 
-Console.WriteLine("\n I am motivated to learn C# because it is a versatile and powerful language that is in high demand in the software industry.Learning C# will expand my skill set and open up various career opportunities in the industry.I am also motivated by the thought of building my own applications.");
+Console.WriteLine(" I am motivated to learn C# because it is a versatile and powerful language that is in high demand in the software industry.Learning C# will help expand my skill set and open up various career opportunities in the industry.");
