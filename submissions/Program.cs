986fda5a5afdb15c2d4a6e43c785999608ce4712
umaidali236 <umaid236@gmail.com>
2024-05-18 18:45:46 +0530

    modified content

---
+++ submissions/Program.cs
@@ -0,0 +1,3 @@
+﻿// Assignment-1
+Console.WriteLine("Hello there ! ");
+Console.WriteLine(" \n This course serves as an introduction to the C# programming language, one of the most widely used languages for developing desktop, web, and mobile applications on the .NET framework.We will learn the fundamentals of C# syntax, data types, control structures, object-oriented programming concepts and common programming techniques. \n\n I am motivated to learn C# because it is a versatile and powerful language that is in high demand in the software industry.Learning C# will expand my skill set and open up various career opportunities in the industry.I am also motivated by the thought of building my own applications. ");
---
+++ submissions/SwapNumbers.cs
@@ -0,0 +1,12 @@
+﻿
+// Assignment-2 #Program to swap two numbers
+
+Console.WriteLine("Enter the value of a  : ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Enter the value of b  : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Before swapping : \n a = {a} \n b = {b}" );
+int temp = a;
+a = b;
+b= temp;
+Console.WriteLine($"After swapping : \n a = {a} \n b = {b}");
